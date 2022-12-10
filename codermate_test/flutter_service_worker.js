3b513074593098f6e61c445110b5ab512c6ac929
Khanh Luong <khanhld1910@gmail.com>
2022-12-10 10:45:19 +0700

    Update

--- codermate_test/flutter_service_worker.js
+++ codermate_test/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "b88baa09cf13427445f84583423bf48d",
-"index.html": "37f020ec37b5d014428bcee1fa2e140a",
-"/": "37f020ec37b5d014428bcee1fa2e140a",
+"index.html": "7b825633f6f6148f8c55c92a4eee55ff",
+"/": "7b825633f6f6148f8c55c92a4eee55ff",
 "main.dart.js": "bc8ae16b94bed4bc528f5641cf9bbf39",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
